91fbaaad584ef66c66943b3cd5e7d0ba6ff90374
nguyenphandiemmy2004-lang <nguyenphandiemmy2004@gmail.com>
2026-06-16 18:14:07 +0700

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.addEventListener(&#39;DOMContentLoaded&#39;, () =&gt; {
+console.log(&#39;Portfolio của Nguyễn Văn A đã load xong!&#39;);
+alert(&#39;Chào mừng bạn đến với portfolio thật trên Internet của mình!&#39;);
+});
